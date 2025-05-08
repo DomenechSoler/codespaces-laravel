82db4caf66221b3f09e0a5cc1493d2de95e0a15d
DomenechSoler <113708519+DomenechSoler@users.noreply.github.com>
2025-05-08 16:56:10 +0000

    api-memory

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+use App\Models\User;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Illuminate\Support\Facades\Auth;
+
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        // Validate the request with validator library
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:100',
+            'role' => 'required|string|max:100|in:admin,user',
+            'email' => 'required|string|email|max:100|unique:users',
+            'password' => 'required|string|min:5|confirmed',
+        ]);
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+        // Create the user
+        $user = User::create([
+            'name' => $request->get('name'),
+            'role' => $request->get('role'),
+            'email' => $request->get('email'),
+            'password' => bcrypt($request->get('password')),
+        ]);
+        // Return the user
+        return response()->json([
+            'message' => 'User created successfully',
+            'data' => $user,
+        ], 201);
+    }
+
+
+    //función para login
+    public function login(Request $request)
+    {
+        // Validate the request with validator library
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|string|email|max:100',
+            'password' => 'required|string|min:5',
+        ]);
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+        $credentials = $request->only(['email', 'password']);
+
+        // Validate the credentials
+        try {
+            if (!$token = JWTAuth::attempt($credentials)) {
+                return response()->json([
+                    'message' => 'Invalid credentials',
+                ], 401);
+            }
+            return response()->json([
+                'message' => 'User logged in successfully',
+                'token' => $token,
+            ], 200);
+        } catch (JWTException $e) {
+            return response()->json([
+                'error'   => 'Could not create token',
+                'message' => $e->getMessage(),
+            ], 500);
+        }
+    }
+
+    //getuser
+    public function getUser()
+    {
+        $user = Auth::user();
+        return response()->json([
+            'message' => 'User retrieved successfully',
+            'data' => $user,
+        ], 200);
+    }
+
+    //logout
+
+    public function logout()
+    {
+        try {
+            JWTAuth::invalidate(JWTAuth::getToken());
+            return response()->json([
+                'message' => 'User logged out successfully',
+            ], 200);
+        } catch (JWTException $e) {
+            return response()->json([
+                'message' => 'Could not log out user',
+            ], 500);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,15 +3,41 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\TarjetasController;
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
+use App\Http\Controllers\AuthController;
+use App\Http\Middleware\IsAdmin;
+use App\Http\Middleware\IsUserAuth;
 
 
-Route::get('/tarjetas', [TarjetasController::class, 'index']);
+
+//PUBLIC ROUTES
+
+Route::get('tarjetas', [TarjetasController::class, 'index']);
+Route::get('tarjetas/{id}', [TarjetasController::class, 'show']);
+Route::post('tarjetas', [TarjetasController::class, 'store']);
+Route::put('tarjetas/{id}', [TarjetasController::class, 'update']);
+Route::delete('tarjetas/{id}', [TarjetasController::class, 'destroy']);
+
+
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+Route::get('tarjetas', [TarjetasController::class, 'index']);
 Route::get('/tarjetas/{id}', [TarjetasController::class, 'show']);
-Route::post('/tarjetas', [TarjetasController::class, 'store']);
-Route::put('/tarjetas/{id}', [TarjetasController::class, 'update']);
-Route::delete('/tarjetas/{id}', [TarjetasController::class, 'destroy']);
+
+//PROTECTED ROUTES
+Route::middleware([IsUserAuth::class])->group(function () {
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::get('me', [AuthController::class, 'getUser']);
+    Route::post('tarjetas', [TarjetasController::class, 'store']);
+});
+
+//ADMIN ROUTES
+Route::middleware([IsAdmin::class])->group(function () {
+
+    Route::get('users', [AuthController::class, 'getUsers']);
+    Route::get('/users/{id}', [AuthController::class, 'getUser']);
+    Route::put('/users/{id}', [AuthController::class, 'updateUser']);
+    Route::delete('/users/{id}', [AuthController::class, 'deleteUser']);
+    Route::post('tarjetas', [TarjetasController::class, 'store']);
+    Route::put('/tarjetas/{id}', [TarjetasController::class, 'update']);
+    Route::delete('/tarjetas/{id}', [TarjetasController::class, 'destroy']);
+});
