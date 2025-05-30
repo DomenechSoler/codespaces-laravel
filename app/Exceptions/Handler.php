86e267a143cc1c6bc68f8d295a0f1b964794201e
DomenechSoler <113708519+DomenechSoler@users.noreply.github.com>
2025-05-30 20:03:53 +0000

    .

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -28,13 +28,16 @@ class Handler extends ExceptionHandler
         //
     }
 
-    protected function unauthenticated($request, AuthenticationException $exception)
-    {
-
-        if ($request->expectsJson()) {
-            return response()->json(['message' => 'Unauthenticated.'], 401);
-        }
-
-        return redirect()->guest(route('login'));
+protected function unauthenticated($request, AuthenticationException $exception)
+{
+    if (
+        $request->expectsJson() ||
+        $request->is('api/*') || 
+        $request->ajax()
+    ) {
+        return response()->json(['message' => 'Unauthenticated.'], 401);
     }
+
+    return redirect()->guest(route('login'));
+}
 }
