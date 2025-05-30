9de2896c98b455bf32d4dc77e285c7e54a4fce9e
DomenechSoler <113708519+DomenechSoler@users.noreply.github.com>
2025-05-30 19:46:35 +0000

    cors

--- config/cors.php
+++ config/cors.php
@@ -2,7 +2,11 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:3000','https://codespaces-laravel-production.up.railway.app'],
+    'allowed_origins' => [
+        'http://localhost:3000',
+        'https://codespaces-laravel-production.up.railway.app',
+        'https://memory-game-3to2.vercel.app',
+    ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
