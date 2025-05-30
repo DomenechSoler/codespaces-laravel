311cfe1f106bc6d9e09302ec901d42f80b7c4eb2
DomenechSoler <113708519+DomenechSoler@users.noreply.github.com>
2025-05-30 18:06:29 +0000

    kernel para cors

---
+++ app/Http/Kernel.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http;
+
+use Illuminate\Foundation\Http\Kernel as HttpKernel;
+
+class Kernel extends HttpKernel
+{
+    /**
+     * The application's global HTTP middleware stack.
+     *
+     * @var array
+     */
+    protected $middleware = [
+
+    ];
+
+    /**
+     * The application's route middleware groups.
+     *
+     * @var array
+     */
+    protected $middlewareGroups = [
+        'web' => [
+            \Illuminate\Cookie\Middleware\EncryptCookies::class,
+            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+            \Illuminate\Session\Middleware\StartSession::class,
+        ],
+
+        'api' => [
+            \Illuminate\Http\Middleware\HandleCors::class,
+            'throttle:api',
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        ],
+    ];
+
+    /**
+     * The application's route middleware.
+     *
+     * @var array
+     */
+    protected $routeMiddleware = [
+        'auth' => \Illuminate\Auth\Middleware\Authenticate::class,
+        'IsUserAuth' => \App\Http\Middleware\IsUserAuth::class,
+        'IsAdmin' => \App\Http\Middleware\IsAdmin::class,
+    ];
+}
