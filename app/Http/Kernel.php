ab8583cf860b6c860bfd3430daf919d10aa49ffb
DomenechSoler <113708519+DomenechSoler@users.noreply.github.com>
2025-05-30 19:36:13 +0000

    c

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -6,21 +6,10 @@ use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
 {
-    /**
-     * The application's global HTTP middleware stack.
-     *
-     * @var array
-     */
     protected $middleware = [
-        \App\Http\Middleware\Cors::class,
-        \Illuminate\Http\Middleware\HandleCors::class,
+
     ];
 
-    /**
-     * The application's route middleware groups.
-     *
-     * @var array
-     */
     protected $middlewareGroups = [
         'web' => [
             \Illuminate\Cookie\Middleware\EncryptCookies::class,
@@ -29,21 +18,15 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            \Illuminate\Http\Middleware\HandleCors::class,
+            \Illuminate\Http\Middleware\HandleCors::class, 
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
 
-    /**
-     * The application's route middleware.
-     *
-     * @var array
-     */
     protected $routeMiddleware = [
         'auth' => \Illuminate\Auth\Middleware\Authenticate::class,
         'IsUserAuth' => \App\Http\Middleware\IsUserAuth::class,
         'IsAdmin' => \App\Http\Middleware\IsAdmin::class,
-        'cors' => \App\Http\Middleware\Cors::class,
     ];
 }
